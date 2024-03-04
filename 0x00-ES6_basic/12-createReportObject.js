f0c6d3c2063a031d51dc53c7cfe06b3f74cda1e5
KENC0DE <kenansameseret@gmail.com>
2024-03-05 02:18:47 +0530

    fix

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,8 +1,11 @@
 export default function createReportObject(employeesList) {
   const report = { allEmployees: {} };
-  for (const dep in employeesList) {
+
+  for (const dep of Object.keys(employeesList)) {
     report.allEmployees[dep] = employeesList[dep];
   }
-  report.getNumberOfDepartments = () => Object.keys(report).length;
+
+  report.getNumberOfDepartments = () => Object.keys(report.allEmployees).length;
+
   return report;
 }
