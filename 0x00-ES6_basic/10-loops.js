e1c1484c85d149a15fd9adfe72109060dbb2eb2a
KENC0DE <kenansameseret@gmail.com>
2024-03-05 00:50:51 +0530

    10

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,7 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
-  for (const idx of array) {
-    let value = array[idx];
-    value = appendString + value;
+  let idx = 0;
+  for (const value of array) {
+    array[idx] = appendString + value;
+    idx += 1;
   }
 
   return array;
