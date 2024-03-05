620042694b9a3b02dbb8bf29e189862f4cef29cb
KENC0DE <kenansameseret@gmail.com>
2024-03-06 01:54:27 +0530

    fixed

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -7,6 +7,6 @@ export default function handleProfileSignup() {
   return Promise.all([photo, user]).then((data) => {
     console.log(`${data[0].body} ${data[1].firstName} ${data[1].lastName}`);
   }).catch(() => {
-    console.log(Error('Signup system offline'));
+    console.log('Signup system offline');
   });
 }
