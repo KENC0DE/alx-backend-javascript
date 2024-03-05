65d94a0524ea10d21ee87484acde7ab9a563f188
KENC0DE <kenansameseret@gmail.com>
2024-03-06 01:42:21 +0530

    task3

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,16 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  const user = createUser();
+  const photo = uploadPhoto();
+
+  photo.then((valp) => {
+    user.then((val) => {
+      console.log(`${valp.body} ${val.firstName} ${val.lastName}`);
+    }).catch(() => {
+      console.log('Signup system offline');
+    });
+  }).catch(() => {
+    console.log('Signup system offline');
+  });
+}
