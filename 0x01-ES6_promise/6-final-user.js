69144f7718655551c7c5ef87ecd52910537eb776
KENC0DE <kenansameseret@gmail.com>
2024-03-06 03:02:21 +0530

    hope

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,20 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const user = signUpUser(firstName, lastName);
+  const photo = uploadPhoto(fileName);
+
+  return Promise.allSettled([user, photo]).then((ans) => {
+    const response = [];
+    if (ans.status === 'fulfilled') {
+      response.push(ans);
+    } else {
+      response.push({
+        status: ans.status,
+        value: `Error: ${ans.message}`,
+      });
+    }
+    return response;
+  });
+}
