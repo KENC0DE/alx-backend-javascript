2c385ec05cfa1997710e94ed252aec7c21081fad
KENC0DE <kenansameseret@gmail.com>
2024-03-06 03:04:40 +0530

    hope

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -12,7 +12,7 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
     } else {
       response.push({
         status: ans.status,
-        value: `Error: ${ans.message}`,
+        value: `Error: ${ans.reason.message}`,
       });
     }
     return response;
