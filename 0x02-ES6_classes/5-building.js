b1e5efaad3c37f0cb5cd67ba49347b8f8e191c27
KENC0DE <kenansameseret@gmail.com>
2024-03-07 15:31:47 +0530

    override

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,6 +1,11 @@
 export default class Building {
   constructor(sqft) {
     this._sqft = sqft;
+
+    if (Object.getPrototypeOf(this).constructor !== Building && 
+        this.evacuationWarningMessage === Building.prototype.evacuationWarningMessage) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
   }
 
   get sqft() {
@@ -9,9 +14,5 @@ export default class Building {
 
   set sqft(val) {
     this._sqft = val;
-  }
-
-  evacuationWarningMessage() {
-    throw new Error('Class extending Building must override evacuationWarningMessage');
   }
 }
