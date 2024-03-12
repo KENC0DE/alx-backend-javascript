d5b3ab882125fd0dc48d51e7ad3a61f2b8e0f692
KENC0DE <kenansameseret@gmail.com>
2024-03-12 20:22:12 +0530

    endpoint

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -3,9 +3,8 @@ export function queryAPI(endp) {
   if (weakMap.get(endp) !== undefined && weakMap.get(endp) >= 5) {
     throw Error('Endpoint load is high');
   }
-  if (weakMap.get(endp) === undefined) {
-    weakMap.set(endp, 1);
-  } else {
-    weakMap.set(endp, weakMap.get(endp) + 1);
+  if (!weakMap.has(endp)) {
+    weakMap.set(endp, 0);
   }
+  weakMap.set(endp, weakMap.get(endp) + 1);
 }
