97d1de2dfed873fc791c6955a8c4dda473721b72
KENC0DE <kenansameseret@gmail.com>
2024-03-12 19:19:04 +0530

    map

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,4 +1,7 @@
 export default function updateUniqueItems(map) {
+  if (typeof (map) !== 'object') {
+    throw Error('Cannot process');
+  }
   for (const [item, q] of map) {
     if (q === 1) {
       map.set(item, 100);
