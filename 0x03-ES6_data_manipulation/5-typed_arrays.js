b4a75ddcd68e0190e449ba15f6beebaf4f69c78a
KENC0DE <kenansameseret@gmail.com>
2024-03-12 16:09:17 +0530

    arrayBuffe

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,9 +1,9 @@
 export default function createInt8TypedArray(length, position, value) {
-  const int8buff = new ArrayBuffer(length);
-
   if (position < 0 || position >= length) {
     return new Error('Position outside range');
   }
+
+  const int8buff = new ArrayBuffer(length);
 
   const array8 = new Int8Array(int8buff);
   array8[position] = value;
