b8cd46fd3660d86c5dad1f8edb5a426b7625bf90
KENC0DE <kenansameseret@gmail.com>
2024-03-13 00:04:36 +0530

    well?

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,8 +1,7 @@
 export default function cleanSet(set, startString) {
-  let result = '';
-  if (!startString || !startString.length) return result;
-  set.forEach((i) => {
-    if (i && i.startsWith(startString)) result += `${i.slice(startString.length)}-`;
-  });
-  return result.slice(0, result.length - 1);
+  if (!startString || !startString.length) return '';
+  return [...set]
+    .filter((el) => el.startsWith(startString))
+    .map((el) => el.slice(startString.length))
+    .join('-');
 }
