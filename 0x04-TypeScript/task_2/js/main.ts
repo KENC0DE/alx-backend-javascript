77b408a7e081c33f2eaf7c38e54d54ca50f2cb6d
KENC0DE <kenansameseret@gmail.com>
2024-03-14 00:54:35 +0530

    director teacher

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -39,9 +39,21 @@ class Teacher implements TeacherInterface {
 }
 
 function createEmployee(salary: number | string): Director | Teacher {
-  if (typeof salary == 'number' && salary < 500) {
+  if (typeof salary === 'number' && salary < 500) {
     return new Teacher();
   }
 
   return new Director();
+}
+
+function isDirector(employee: Director | Teacher): boolean {
+  return employee instanceof Director;
+}
+
+function executeWork(employee: Director | Teacher): void {
+  if (isDirector(employee)) {
+    console.log((employee as Director).workDirectorTasks());
+  } else {
+    console.log((employee as Teacher).workTeacherTasks());
+  }
 }
