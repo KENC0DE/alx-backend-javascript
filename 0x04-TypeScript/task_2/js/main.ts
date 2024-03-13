73f300d8b80ff1357ec51590a8bbf62ee2ab227b
KENC0DE <kenansameseret@gmail.com>
2024-03-14 01:02:25 +0530

    math and History

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -57,3 +57,16 @@ function executeWork(employee: Director | Teacher): void {
     console.log((employee as Teacher).workTeacherTasks());
   }
 }
+
+type Subjects = 'Math' | 'History';
+
+function teachClass(todayClass: Subjects): string {
+  if (todayClass === 'Math') {
+    return 'Teaching Math';
+  }
+
+  return 'Teaching History';
+}
+
+console.log(teachClass('Math'));
+console.log(teachClass('History'));
