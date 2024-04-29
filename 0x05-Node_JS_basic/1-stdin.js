8f62b2143982265a4b1c8aede810abf190d6a1cb
KENC0DE <kenansameseret@gmail.com>
2024-04-30 01:02:27 +0530

    -fix

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,11 +1,8 @@
 console.log('Welcome to Holberton School, what is your name?');
 
-process.stdin.resume();
-
 process.stdin.on('data', (chunk) => {
   const name = chunk.toString().trim();
   console.log(`Your name is: ${name}`);
-  process.exit();
 });
 
 process.stdin.on('end', () => {
