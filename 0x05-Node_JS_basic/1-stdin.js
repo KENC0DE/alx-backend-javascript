94657311b67a991345a9ede1e6de5fb600b5ba14
KENC0DE <kenansameseret@gmail.com>
2024-04-30 00:48:19 +0530

    c

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -15,4 +15,8 @@ process.stdin.on('data', (chunk) => {
     console.log('This important software is now closing');
     process.exit();
   });
+
+  process.on('exit', () => {
+    console.log('This important software is now closing');
+  });
 });
