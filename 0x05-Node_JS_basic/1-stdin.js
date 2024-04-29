337dc60fd68c3e45b8988a00bbf73c758941f57b
KENC0DE <kenansameseret@gmail.com>
2024-04-30 00:57:09 +0530

    isatty

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -5,13 +5,8 @@ process.stdin.resume();
 process.stdin.on('data', (chunk) => {
   const name = chunk.toString().trim();
   console.log(`Your name is: ${name}`);
+});
 
-  if (!process.stdin.isTTY) {
-    console.log('This important software is now closing');
-    process.exit();
-  }
-
-  process.on('exit', () => {
-    console.log('This important software is now closing');
-  });
+process.on('exit', () => {
+  console.log('This important software is now closing');
 });
